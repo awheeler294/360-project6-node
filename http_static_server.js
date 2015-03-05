ab1b1d4f47baa98fb4e4d47f56a22910448e0ce7
andrew <awheeler294@gmail.com>
2015-03-05 14:15:02 -0700

    a

--- http_static_server.js
+++ http_static_server.js
@@ -12,9 +12,9 @@ http.createServer(function (request, response) {
          }
          else {
             var cities = data.toString().split("\n");
-            // for(var i = 0; i < cities.length; i++) {
-            //    console.log(cities[i]);
-            // }
+            for(var i = 0; i < cities.length; i++) {
+               console.log(cities[i]);
+            }
             var myRe = new RegExp("^" + urlObj.query["q"]);
             var jsonresult = [];
             for(var i = 0; i < cities.length; i++) {
