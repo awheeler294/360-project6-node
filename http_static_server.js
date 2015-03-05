b84035d43043d6484662ef2afab5ed69143503e8
andrew <awheeler294@gmail.com>
2015-03-05 14:32:35 -0700

    I'm alive

--- http_static_server.js
+++ http_static_server.js
@@ -12,14 +12,17 @@ http.createServer(function (request, response) {
          }
          else {
             var cities = data.toString().split("\n");
-            for(var i = 0; i < cities.length; i++) {
-               console.log(cities[i]);
-            }
+            // for(var i = 0; i < cities.length; i++) {
+            //    console.log(cities[i]);
+            // }
             var myRe = new RegExp("^" + urlObj.query["q"]);
             var jsonresult = [];
             for(var i = 0; i < cities.length; i++) {
-               var result = cities[i].search(myRe); 
+               console.log("I'm alive!");
+               var result = cities[i].search(myRe);
+               console.log("I'm alive!"); 
                if(result != -1) {
+                  console.log("I'm alive!");
                   console.log(cities[i]);
                   jsonresult.push({city:cities[i]});
                } 
