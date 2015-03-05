a4ebac0777d9c1b980a3fc620ff5c890f25ed132
andrew <ubuntu@ip-172-31-45-121.us-west-2.compute.internal>
2015-03-05 16:26:05 +0000

    First Commit

---
+++ http_static_server.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+var ROOT_DIR = "html/";
+http.createServer(function (request, response) {
+   var urlObj = url.parse(request.url, true, false);
+   if (urlObj.pathname.indexOf("getcity") != -1) {
+      console.log("In Getcity");
+      fs.readFile("cities.dat.txt", function (err, data) {
+         if (err) {
+            throw err;
+         }
+         else {
+            var cities = data.toString().split("/n");
+            var myRe = new RegExp("^" + urlObj.query["q"]);
+            var jsonresult = [];
+            
+            for(var i = 0; i < cities.length; i++) {
+               var result = cities[i].search(myRe); 
+               if(result != -1) {
+                  console.log(cities[i]);
+                  jsonresult.push({city:cities[i]});
+               } 
+            }
+
+            console.log(JSON.stringify(jsonresult));
+            response.writeHead(200);
+            response.end(JSON.stringify(jsonresult));
+         }
+      });
+      
+   }
+   else {
+      fs.readFile(ROOT_DIR + urlObj.pathname, function (err, data) {
+         if (err) {
+            response.writeHead(404);
+            response.end(JSON.stringify(err));
+            return;
+         }
+         response.writeHead(200);
+         response.end(data);
+      });
+   }
+}).listen(80);
