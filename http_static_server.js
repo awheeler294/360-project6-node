66b382ff4922aa48871912c1a1159a398b124798
andrew <awheeler294@gmail.com>
2015-03-05 14:46:14 -0700

    added case-insensitive

--- http_static_server.js
+++ http_static_server.js
@@ -15,7 +15,7 @@ http.createServer(function (request, response) {
             // for(var i = 0; i < cities.length; i++) {
             //    console.log(cities[i]);
             // }
-            var myRe = new RegExp("^" + urlObj.query["q"]);
+            var myRe = new RegExp("^" + urlObj.query["q"] + "i");
             // console.log(myRe);
             var jsonresult = [];
             for(var i = 0; i < cities.length; i++) {
