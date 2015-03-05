efd8153b6068ed483a2ded66d7c6928f1b4e38ac
andrew <awheeler294@gmail.com>
2015-03-05 14:06:38 -0700

    I dont want to always have to enter a commit message!

--- http_static_server.js
+++ http_static_server.js
@@ -13,15 +13,15 @@ http.createServer(function (request, response) {
          else {
             var cities = data.toString().split("/n");
             var myRe = new RegExp("^" + urlObj.query["q"]);
-            var jsonresult = "";
-            
+            var jsonresult = [];
             for(var i = 0; i < cities.length; i++) {
                var result = cities[i].search(myRe); 
                if(result != -1) {
                   console.log(cities[i]);
                   jsonresult.push({city:cities[i]});
                } 
-            }
+            }   
+            console.log(jsonresult);
 
             console.log(JSON.stringify(jsonresult));
             response.writeHead(200);
